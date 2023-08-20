a4dd563e8a750463ae70b0d85ad612372eb2ae4f
Dale Millington <dkmillington@tutanota.com>
2023-08-20 15:27:48 +0100

    Chart UI now in place

--- frontend/src/components/WeatherCharts.tsx
+++ frontend/src/components/WeatherCharts.tsx
@@ -40,8 +40,22 @@ const WeatherCharts = ({ activeCity, clearActiveCity }: WeatherChartsProps) => {
     return (
         <>
             <div className="map-barrier">
-                <div>Title</div>
-                <div>Back</div>
+                <div className="chart-row chart-row--header">
+                    <div className="chart-title-element chart-title-element--left">{ activeCity?.name }</div>
+                    <div className="chart-title-element chart-title-element--right">
+                        <button className="chart-back" onClick={ clearActiveCity }>
+                            <span className="chart-back__icon">&larr;</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="chart-row chart-row--main">
+                    <div className="chart-container-element chart-container-element--left-1"></div>
+                    <div className="chart-container-element chart-container-element--right-1"></div>
+                </div>
+                <div className="chart-row chart-row--main">
+                    <div className="chart-container-element chart-container-element--left-2"></div>
+                    <div className="chart-container-element chart-container-element--right-2"></div>
+                </div>
             </div>
             <p onClick={clearActiveCity}>{activeCity?.name}</p>
         </>
